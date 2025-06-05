467f1dfe934d624c52427d7b25066f4277ede612
Anderson L <113462710+andersonlsouza@users.noreply.github.com>
2025-06-04 23:23:54 -0300

    feat: add doctor add doctor creation form

--- src/app/(protected)/doctors/page.tsx
+++ src/app/(protected)/doctors/page.tsx
@@ -1,8 +1,6 @@
-import { Plus } from "lucide-react";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-import { Button } from "@/components/ui/button";
 import {
   PageActions,
   PageContainer,
@@ -13,6 +11,8 @@ import {
   PageTitle,
 } from "@/components/ui/page-container";
 import { auth } from "@/lib/auth";
+
+import AddButtonDoctor from "./_components/add-button-doctor";
 
 const DoctorsPage = async () => {
   const session = await auth.api.getSession({
@@ -35,10 +35,7 @@ const DoctorsPage = async () => {
           <PageDescription>Gerencie os médicos de sua clínica.</PageDescription>
         </PageHeaderContent>
         <PageActions>
-          <Button>
-            <Plus />
-            Adicionar médico
-          </Button>
+          <AddButtonDoctor />
         </PageActions>
       </PageHeader>
       <PageContent>
