32e26a80697439de318f51d3480031303ac426ef
Anderson Lima <andersonlimasouza375@gmail.com>
2025-10-14 18:46:24 -0300

    feat: add new dependencies and appointments price field

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -173,6 +173,7 @@ export const patientsTableRelations = relations(
 export const appointmentsTable = pgTable("appointments", {
   id: uuid("id").defaultRandom().primaryKey(),
   date: timestamp("date").notNull(),
+  appointmentsPriceInCents: integer("appointments_price_in_cents").notNull(),
   patientId: uuid("patient_id")
     .notNull()
     .references(() => patientsTable.id),
